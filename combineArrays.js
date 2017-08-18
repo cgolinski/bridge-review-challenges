ca346e22c898d69bce3955a4c428301d12574419
cgolinski <cgolinski@users.noreply.github.com>
2017-08-18 17:38:36 -0400

    combineArrays

---
+++ combineArrays.js
@@ -0,0 +1,22 @@
+/*
+Write a function that combines two arrays of strings, removing duplicates (case insensitive), and ordering the strings from smallest to biggest, ie:
+
+[‘Hello’, ‘World’, ‘My’, ‘Name’, ‘world’, ‘Is’, ‘my’, ‘Hello’, ‘Della’];
+
+Should result in this (or something of similar length ordering)
+
+[‘Is’, ‘My’, ‘Name’, ‘Della’, ‘Hello’, ‘World’]
+
+*/
+
+const array1 = ['Hello', 'World', 'My', 'Name'];
+const array2 = ['world', 'Is', 'my', 'Hello', 'Della'];
+
+const combineArraysThenSort = (arr1, arr2) => []
+  .concat(arr1, arr2)
+  .map((str) => str.toLowerCase())
+  .filter((str, index, array) => array.indexOf(str) === index)
+  .sort((a, b) => a.length - b.length);
+
+console.log(combineArraysThenSort(array1, array2));
+
